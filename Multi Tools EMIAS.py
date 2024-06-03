53d791e4b18c9ad7fd3c9cd09adcef96b0a5e41f
Aleksey Shubenkov <shubenkov-93@yandex.ru>
2024-06-03 15:20:19 +0300

    Рефакторинг кода 2

--- Multi Tools EMIAS.py
+++ Multi Tools EMIAS.py
@@ -3,8 +3,8 @@ from PyQt5.QtWidgets import QApplication
 
 
 class Ui_Window(object):
-
     def create_button(self, parent, b_size, icon_path, i_size=None):
+        """Создание кнопок."""
         button = QtWidgets.QPushButton(parent)
         button.setGeometry(QtCore.QRect(*b_size))
         button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
@@ -15,6 +15,24 @@ class Ui_Window(object):
             button.setIconSize(QtCore.QSize(*i_size))
         button.setIcon(icon)
         return button
+
+    def create_widget(self, parent, size, widget_type, bold=False):
+        """Создание виджетов."""
+        widget_types = {
+            "line": QtWidgets.QFrame,
+            "lineEdit": QtWidgets.QLineEdit,
+            "label": QtWidgets.QLabel,
+            "textEdit": QtWidgets.QTextEdit
+        }
+        widget = widget_types.get(widget_type)(parent)
+        widget.setGeometry(*size)
+
+        if widget_type == "line":
+            widget.setFrameShape(QtWidgets.QFrame.HLine)
+            widget.setFrameShadow(QtWidgets.QFrame.Sunken)
+        elif widget_type == "label" and bold:
+            widget.setFont(self.font)
+        return widget
 
     def setupUi(self, Window):
         # Параметры окна
@@ -38,8 +56,8 @@ class Ui_Window(object):
         self.tab_2 = QtWidgets.QWidget()
         self.Main.addTab(self.tab_2, "")
 
-        font = QtGui.QFont()
-        font.setPointSize(14)
+        self.font = QtGui.QFont()
+        self.font.setPointSize(14)
 
         # Выпадающий список моделей принтера
         self.combo_model = QtWidgets.QComboBox(self.tab)
@@ -49,65 +67,93 @@ class Ui_Window(object):
         for _ in range(8):
             self.combo_model.addItem("")
 
-        self.lineEdit_ip = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit_ip.setGeometry(QtCore.QRect(60, 55, 101, 22))
+        # QFrame (Линии)
+        self.line_print_1 = self.create_widget(
+            self.tab,
+            (10, 30, 403, 21),
+            "line"
+        )
+        self.line_print_2 = self.create_widget(
+            self.tab,
+            (10, 80, 403, 21),
+            "line",
+        )
+        self.line_scan_hp = self.create_widget(
+            self.tab_2,
+            (10, 30, 401, 21),
+            "line",
+        )
 
-        self.lbl_ip = QtWidgets.QLabel(self.tab)
-        self.lbl_ip.setGeometry(QtCore.QRect(10, 60, 47, 13))
+        # QLineEdit
+        self.lineEdit_ip = self.create_widget(
+            self.tab,
+            (60, 55, 101, 22),
+            "lineEdit"
+        )
+        self.lineEdit_cnt = self.create_widget(
+            self.tab,
+            (364, 55, 22, 22),
+            "lineEdit"
+        )
+        self.lineEdit_login = self.create_widget(
+            self.tab_2,
+            (11, 67, 131, 22),
+            "lineEdit"
+        )
+        self.lineEdit_ip_arm = self.create_widget(
+            self.tab_2,
+            (11, 152, 131, 22),
+            "lineEdit"
+        )
+        self.lineEdit_hostname = self.create_widget(
+            self.tab_2,
+            (11, 110, 131, 22),
+            "lineEdit"
+        )
 
-        self.lbl_model = QtWidgets.QLabel(self.tab)
-        self.lbl_model.setGeometry(QtCore.QRect(198, 60, 47, 13))
-
-        # Линии
-        self.line_print_1 = QtWidgets.QFrame(self.tab)
-        self.line_print_1.setGeometry(QtCore.QRect(10, 30, 403, 21))
-        self.line_print_1.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_print_1.setFrameShadow(QtWidgets.QFrame.Sunken)
-
-        self.line_print_2 = QtWidgets.QFrame(self.tab)
-        self.line_print_2.setGeometry(QtCore.QRect(10, 80, 403, 21))
-        self.line_print_2.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_print_2.setFrameShadow(QtWidgets.QFrame.Sunken)
-
-        self.line_scan_hp = QtWidgets.QFrame(self.tab_2)
-        self.line_scan_hp.setGeometry(QtCore.QRect(10, 30, 401, 21))
-        self.line_scan_hp.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_scan_hp.setFrameShadow(QtWidgets.QFrame.Sunken)
-
-
-        self.lbl_print = QtWidgets.QLabel(self.tab)
-        self.lbl_print.setGeometry(QtCore.QRect(10, 0, 151, 41))
-        self.lbl_print.setFont(font)
-
-        self.lbl_scan_hp = QtWidgets.QLabel(self.tab_2)
-        self.lbl_scan_hp.setGeometry(QtCore.QRect(10, 0, 151, 41))
-        self.lbl_scan_hp.setFont(font)
-
-
-
-        self.lineEdit_cnt = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit_cnt.setGeometry(QtCore.QRect(364, 55, 22, 22))
-
-        self.lineEdit_ip_arm = QtWidgets.QLineEdit(self.tab_2)
-        self.lineEdit_ip_arm.setGeometry(QtCore.QRect(11, 152, 131, 22))
-
-        self.lineEdit_login = QtWidgets.QLineEdit(self.tab_2)
-        self.lineEdit_login.setGeometry(QtCore.QRect(11, 67, 131, 22))
-
-        self.lbl_hostname = QtWidgets.QLabel(self.tab_2)
-        self.lbl_hostname.setGeometry(QtCore.QRect(13, 92, 105, 16))
-
-        self.lbl_ip_arm = QtWidgets.QLabel(self.tab_2)
-        self.lbl_ip_arm.setGeometry(QtCore.QRect(13, 134, 71, 16))
-
-        self.lbl_login = QtWidgets.QLabel(self.tab_2)
-        self.lbl_login.setGeometry(QtCore.QRect(13, 48, 97, 16))
-
-        self.lineEdit_hostname = QtWidgets.QLineEdit(self.tab_2)
-        self.lineEdit_hostname.setGeometry(QtCore.QRect(11, 110, 131, 22))
-
-        self.textEdit = QtWidgets.QTextEdit(self.tab_2)
-        self.textEdit.setGeometry(QtCore.QRect(211, 67, 200, 106))
+        # QLabel
+        self.lbl_print = self.create_widget(
+            self.tab,
+            (10, 0, 151, 41),
+            "label",
+            True
+        )
+        self.lbl_scan_hp = self.create_widget(
+            self.tab_2,
+            (10, 0, 151, 41),
+            "label",
+            True
+        )
+        self.lbl_ip = self.create_widget(
+            self.tab,
+            (10, 60, 47, 13),
+            "label"
+        )
+        self.lbl_model = self.create_widget(
+            self.tab,
+            (198, 60, 47, 13),
+            "label"
+        )
+        self.lbl_hostname = self.create_widget(
+            self.tab_2,
+            (13, 92, 105, 16),
+            "label"
+        )
+        self.lbl_ip_arm = self.create_widget(
+            self.tab_2,
+            (13, 134, 71, 16),
+            "label"
+        )
+        self.lbl_login = self.create_widget(
+            self.tab_2,
+            (13, 48, 97, 16),
+            "label"
+        )
+        self.textEdit = self.create_widget(
+            self.tab_2,
+            (211, 67, 200, 106),
+            "textEdit"
+        )
 
         # Кнопка "Копировать"
         self.btn_copy = self.create_button(
@@ -118,37 +164,37 @@ class Ui_Window(object):
         self.btn_copy.clicked.connect(self.send_clipboard_printer)
 
         # Кнопка "Удалить IP"
-        self.btn_clear_ip = self.create_button(
+        self.btn_clr_ip = self.create_button(
             self.tab,
-            [163, 54, 24, 24],
+            (163, 54, 24, 24),
             "icons/icon_delete.png"
         )
-        self.btn_clear_ip.clicked.connect(self.lineEdit_ip.clear)
+        self.btn_clr_ip.clicked.connect(self.lineEdit_ip.clear)
 
         # Кнопка "Очистить очередь печати"
-        self.btn_claer_spooler = self.create_button(
+        self.btn_clr_spooler = self.create_button(
             self.tab,
-            [44, 100, 38, 40],
+            (44, 100, 38, 40),
             "icons/icon_clear.png",
-            [35, 35]
+            (35, 35)
         )
-        self.btn_claer_spooler.clicked.connect(self.send_clipboard_clr_spooler)
+        self.btn_clr_spooler.clicked.connect(self.send_clipboard_clr_spooler)
 
         # Кнопка "Печать тестовой страницы"
         self.btn_test_page = self.create_button(
             self.tab,
-            [87, 100, 37, 40],
+            (87, 100, 37, 40),
             "icons/icon_test_page.png",
-            [32, 32]
+            (32, 32)
         )
         self.btn_test_page.clicked.connect(self.send_clipboard_test_page)
 
         # Кнопка "Удаления принтера"
         self.btn_del_printer = self.create_button(
             self.tab,
-            [129, 100, 38, 40],
+            (129, 100, 38, 40),
             "icons/icon_delete_printer.png",
-            [32, 32]
+            (32, 32)
         )
         self.btn_del_printer.clicked.connect(self.send_clipboard_del_printer)
 
@@ -227,7 +273,7 @@ class Ui_Window(object):
             (151, 110, 24, 24),
             "icons/icon_delete.png"
         )
-        self.btn_clear_hostname.clicked.connect(self.clear_hostname_hp)
+        self.btn_clear_hostname.clicked.connect(self.lineEdit_hostname.clear)
 
         # Кнопка "Очистить IP"
         self.btn_clear_ip_scan_hp = self.create_button(
@@ -235,12 +281,12 @@ class Ui_Window(object):
             (151, 150, 24, 24),
             "icons/icon_delete.png"
         )
-        self.btn_clear_ip_scan_hp.clicked.connect(self.clear_ip_hp)
+        self.btn_clear_ip_scan_hp.clicked.connect(self.lineEdit_ip_arm.clear)
 
         self.quickWidget = QtQuickWidgets.QQuickWidget(self.centralwidget)
         self.quickWidget.setGeometry(QtCore.QRect(-210, 910, 300, 200))
-        self.quickWidget.setResizeMode(QtQuickWidgets.QQuickWidget.SizeRootObjectToView)
-
+        self.quickWidget.setResizeMode(
+            QtQuickWidgets.QQuickWidget.SizeRootObjectToView)
         Window.setCentralWidget(self.centralwidget)
         self.actionExit = QtWidgets.QAction(Window)
         self.retranslateUi(Window)
@@ -248,8 +294,8 @@ class Ui_Window(object):
         QtCore.QMetaObject.connectSlotsByName(Window)
 
     def combobox_menu(self):
-        """Комбобокс для моделей."""
-        models = [
+        """Выпадающий список для моделей."""
+        models = (
             "HP LaserJet M401",
             "HP LaserJet M404",
             "HP LaserJet M501",
@@ -257,13 +303,12 @@ class Ui_Window(object):
             "HP LaserJet M426",
             "Pantum BP5100",
             "Pantum BM5100"
-        ]
+        )
         for idx, model in enumerate(models, start=1):
             self.combo_model.setItemText(idx, model)
 
     def title_tip(self):
         """Надписи и подсказки."""
-        _translate = QtCore.QCoreApplication.translate
         titles = {
             self.lbl_ip: "IP адрес:",
             self.lbl_model: "Модель:",
@@ -275,9 +320,9 @@ class Ui_Window(object):
         }
         tips = {
             self.btn_copy: "Копировать в буфер",
-            self.btn_clear_ip: "Очистить поле",
+            self.btn_clr_ip: "Очистить поле",
             self.lineEdit_cnt: "Номер принтера",
-            self.btn_claer_spooler: "Очистить очередь печати",
+            self.btn_clr_spooler: "Очистить очередь печати",
             self.btn_test_page: "Тестовая страница",
             self.btn_del_printer: "Удалить принтер",
             self.btn_reboot: "Перезагрузка АРМ",
@@ -294,26 +339,26 @@ class Ui_Window(object):
 
         for widget, text in {**titles, **tips}.items():
             if isinstance(widget, QtWidgets.QLabel):
-                widget.setText(_translate("Window", text))
+                widget.setText(self._translate("Window", text))
             else:
-                widget.setToolTip(_translate("Window", text))
-
-
+                widget.setToolTip(self._translate("Window", text))
 
     def retranslateUi(self, Window):
-        _translate = QtCore.QCoreApplication.translate
-        Window.setWindowTitle(_translate("Window", "Multi Tools EMIAS"))
+        self._translate = QtCore.QCoreApplication.translate
+        Window.setWindowTitle(self._translate("Window", "Multi Tools EMIAS"))
         Window.setWindowIcon(QtGui.QIcon('emias.ico'))
         self.combobox_menu()
         self.title_tip()
-        self.Main.setTabText(self.Main.indexOf(self.tab),
-                             _translate("Window", "Печать"))
-        self.Main.setTabText(self.Main.indexOf(self.tab_2),
-                             _translate("Window", "Сканирование (HP)"))
-        self.actionExit.setText(_translate("Window", "Exit"))
-
+        self.Main.setTabText(
+            self.Main.indexOf(self.tab),
+            self._translate("Window", "Печать"))
+        self.Main.setTabText(
+            self.Main.indexOf(self.tab_2),
+            self._translate("Window", "Сканирование (HP)"))
+        self.actionExit.setText(self._translate("Window", "Exit"))
 
     def send_clipboard_printer(self):
+        """Команда для установки принтера."""
         ip = self.lineEdit_ip.text().replace(" ", "")
         folder_1 = "/opt/Printer_Drivers/Ochered/4-ya ochered/"
         folder_2 = "/home/admin/"
@@ -326,22 +371,21 @@ class Ui_Window(object):
             "BP5100": f"{folder_2}PANTUM_5100.ppd",
             "BM5100": f"{folder_2}PANTUM_5100.ppd"
         }
-        cnt = ''
+        cnt = ""
         if self.lineEdit_cnt.text() != '':
             cnt = '-' + self.lineEdit_cnt.text()
-        models = self.combo_model.currentText().replace(' ', '-').replace('Pantum-', '')
-        model = models
+        model = self.combo_model.currentText().replace(' ', '-').replace('Pantum-', '')
         ftp = ("wget -nc /home/admin "
                "ftp://printer:z123456Z"
                "@srv-ftp02/13_ochered/PPD/PANTUM_5100.ppd")
-        models += cnt
-        if models:
-            install = (f"/usr/sbin/lpadmin -p '{models}' -v "
+        if model:
+            install = (f"/usr/sbin/lpadmin -p "
+                       f"'{model + cnt if cnt else model}' -v "
                        f"'socket://{ip}:9100' -P '{drivers[model]}'")
-            enable = f"/usr/sbin/cupsenable {models}"
-            accept = f"/usr/sbin/cupsaccept {models}"
-            default = f"/usr/sbin/lpadmin -d {models}"
-            options = f"lpoptions -d {models}"
+            enable = f"/usr/sbin/cupsenable {model}"
+            accept = f"/usr/sbin/cupsaccept {model}"
+            default = f"/usr/sbin/lpadmin -d {model}"
+            options = f"lpoptions -d {model}"
             test_page = "lpr /usr/share/cups/data/default-testpage.pdf"
             status = "lpstat -t"
             commands = [
@@ -359,60 +403,65 @@ class Ui_Window(object):
             QApplication.clipboard().setText(command)
 
     def send_clipboard_clr_spooler(self):
+        """Команда для чистки очереди печати."""
         command = "lprm -;lpstat -t"
         QApplication.clipboard().setText(command)
 
     def send_clipboard_test_page(self):
+        """Команда для печати тестовой страницы с принтера."""
         command = "lpr /usr/share/cups/data/default-testpage.pdf;lpstat -t"
         QApplication.clipboard().setText(command)
 
     def send_clipboard_del_printer(self):
+        """Команда для удаления принтера."""
         command = "/usr/sbin/lpadmin -x "
         QApplication.clipboard().setText(command)
 
     def send_clipboard_reboot(self):
+        """Команда для перезагрузки АРМ."""
         command = "systemctl reboot -i"
         QApplication.clipboard().setText(command)
 
     def send_clipboard_cups(self):
+        """Команда для перезагрузки службы CUPS."""
         command = "systemctl restart cups"
         QApplication.clipboard().setText(command)
 
     def send_clipboard_default(self):
+        """Команда для устаовки принтера по умолчанию."""
         command = "/usr/sbin/lpadmin -d "
         QApplication.clipboard().setText(command)
 
     def send_clipboard_rename(self):
+        """Команда для смены 'hostname' АРМ."""
         command = "hostnamectl set-hostname "
         QApplication.clipboard().setText(command)
 
     def send_clipboard_password(self):
+        """Пароль."""
         command = "o123456O"
         QApplication.clipboard().setText(command)
 
     def send_clipboard_hostname(self):
-        login = self.lineEdit_login.text().replace(" ", "")
-        hostname = self.lineEdit_hostname.text().replace(" ", "")
-        command = f"\\\{hostname}.mosgorzdrav.local\scan\{login}"
+        """Команда для настройки сканирования через 'hostname' АРМ."""
+        login = self.lineEdit_login.text()
+        hostname = self.lineEdit_hostname.text()
+        command = (fr"\{hostname}.mosgorzdrav.local\scan\{login}"
+                   .replace(" ", ""))
         QApplication.clipboard().setText(command)
 
     def send_clipboard_ip_arm(self):
+        """Команда для настройки сканирования через 'ip-адрес' АРМ."""
         login = self.lineEdit_login.text()
         ip = self.lineEdit_ip_arm.text()
-        command = f"\\\{ip}\scan\{login}".replace(" ", "")
+        command = fr"\{ip}\scan\{login}".replace(" ", "")
         QApplication.clipboard().setText(command)
-
-    def clear_hostname_hp(self):
-        self.lineEdit_hostname.clear()
-
-    def clear_ip_hp(self):
-        self.lineEdit_ip_arm.clear()
 
     def clear_all_scan(self):
         self.lineEdit_login.clear()
         self.textEdit.clear()
-        self.clear_ip_hp()
-        self.clear_hostname_hp()
+        self.lineEdit_ip_arm.clear()
+        self.lineEdit_hostname.clear()
 
 
 if __name__ == '__main__':
